1c39e7a640fb7d9ef8cd39b7471ad939718d4665
hridaewcca <35859521+hridaewcca@users.noreply.github.com>
2018-01-26 22:52:17 -0800

    Create puzzle-9.js

---
+++ puzzle-9.js
@@ -0,0 +1,48 @@
+function moveRight (){
+	var steps = 0
+		while (steps < 8){
+			var A = getColor ()
+			while (getColor == "white") {
+				setColor ("blue")
+				right
+				}
+			steps = steps + 1
+		}
+}
+function moveDown (){				
+	var steps = 0
+		while (steps < 4){
+			var A = getColor ()
+			while (getColor == "white") {
+				setColor ("blue")
+				down
+				}
+			steps = steps + 1
+		}
+}
+function moveLeft (){				
+	var steps = 0
+		while (steps < 8){
+			var A = getColor ()
+			while (getColor == "white") {
+				setColor ("blue")
+				left
+				}
+			steps = steps + 1
+		}
+}
+function moveUp (){				
+	var steps = 0
+		while (steps < 3){
+			var A = getColor ()
+			while (getColor == "white") {
+				setColor ("blue")
+				up
+				}
+			steps = steps + 1
+		}
+}
+moveRight
+moveDown
+moveLeft
+moveUp
