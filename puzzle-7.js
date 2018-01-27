2dd4133067eb034dadeb40dcdd1a143cb10d4a5f
hridaewcca <35859521+hridaewcca@users.noreply.github.com>
2018-01-26 22:13:15 -0800

    Create puzzle-7.js

---
+++ puzzle-7.js
@@ -0,0 +1,21 @@
+function lineDown (){
+  while (getColor !="red") {
+      down
+  }
+  right
+}
+function lineUp (){
+  while (getColor !="red") {
+    up
+  }
+  right
+}
+lineDown
+right
+lineUp
+right
+lineDown
+right
+lineUp
+right
+lineDown
