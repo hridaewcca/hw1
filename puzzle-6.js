dae2f94c7d1e2ea85f3447a5e011e2a020592e76
hridaewcca <35859521+hridaewcca@users.noreply.github.com>
2018-01-26 22:02:27 -0800

    Create puzzle-6.js

---
+++ puzzle-6.js
@@ -0,0 +1,22 @@
+function pinkAction (){
+  up
+  up
+  up
+    if (getColor == "blue") {
+      right
+      up
+    }
+    if (getColor == "pink") {
+      left
+      up
+    }
+}
+right
+  if (getColor == "blue") {
+    down
+    down
+    down
+    left
+    down
+  }
+else pinkAction
