ae7e09c8cfc86b2f217fd89365ab62cd221d4507
hridaewcca <35859521+hridaewcca@users.noreply.github.com>
2018-01-26 22:27:08 -0800

    Create puzzle-8.js

---
+++ puzzle-8.js
@@ -0,0 +1,25 @@
+function goDown (){
+  var steps = 0 ()
+  while (steps < 8) {
+    down
+    steps = steps + 1
+  }
+  right
+}
+function goUp (){
+  var steps = 0 ()
+  while (steps < 8) {
+    up
+    steps = steps + 1
+  }
+  right
+}
+goDown
+right
+goUp
+right
+goDown
+right
+goUp
+right
+goDown
