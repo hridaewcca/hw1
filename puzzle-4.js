d620c767ce3c7370de0189fb1d49b2cb371feec1
hridaewcca <35859521+hridaewcca@users.noreply.github.com>
2018-01-29 09:22:34 -0800

    Update puzzle-4.js

--- puzzle-4.js
+++ puzzle-4.js
@@ -1,16 +1,16 @@
 function PassThrough (){
-up
-right
-var A = setColor ()
-down
-setColor(A)
-right
-right
+up();
+right();
+var A = getColor ();
+down();
+setColor(A);
+right();
+right();
 }
-down
-down
-down
-down
-down
-PassThrough
-PassThrough
+down();
+down();
+down();
+down();
+down();
+PassThrough();
+PassThrough();
